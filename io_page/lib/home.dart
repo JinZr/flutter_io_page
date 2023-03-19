e4b6da2ce0a12db69b617c6153fc4c95077e65d6
jinzr <60612200+JinZr@users.noreply.github.com>
2023-03-19 17:24:08 +0800

    updated

--- io_page/lib/home.dart
+++ io_page/lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 
 import 'home_components/self_intro_card.dart';
@@ -22,35 +23,134 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    final body = <Widget>[
-      _buildGridViewPanel(),
-      const FullPublicationView(title: "Publications"),
-      const PolaroidGalleryView(title: "Polaroid Cameras"),
-      // const DatasetView(title: "Datasets"),
-    ];
-
-    return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-            appBar: AppBar(
-              title: Text(widget.titleEn),
-              bottom: const TabBar(tabs: [
-                Tab(icon: Icon(Icons.home)),
-                Tab(icon: Icon(Icons.library_books)),
-                Tab(icon: Icon(Icons.camera_alt_sharp))
-              ]),
-            ),
-            body: TabBarView(children: body)));
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (MediaQuery.of(context).size.width <
+            MediaQuery.of(context).size.height) {
+          return MobileLayout(
+            titleEn: widget.titleEn,
+            titleZh: widget.titleZh,
+          );
+        } else {
+          return DesktopLayout(
+            titleEn: widget.titleEn,
+            titleZh: widget.titleZh,
+          );
+        }
+      },
+    );
   }
+}
 
-  Widget _buildGridViewPanel() => ConstrainedBox(
-      constraints: const BoxConstraints(maxWidth: 1200),
-      child: ListView(
-        children: const [
-          IntroductionCard(),
-          UpdateCard(),
-          PublicationCard(),
-          PolaroidCard()
-        ],
-      ));
+class MobileLayout extends StatelessWidget {
+  final String titleEn;
+  final String titleZh;
+  final _panel = Center(
+    child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 1200),
+        child: ListView(
+          children: const [
+            IntroductionCard(),
+            UpdateCard(),
+            PublicationCard(),
+            PolaroidCard()
+          ],
+        )),
+  );
+
+  MobileLayout({Key? key, required this.titleEn, required this.titleZh})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(titleEn),
+            bottom: const TabBar(tabs: [
+              Tab(icon: Icon(Icons.home)),
+              Tab(icon: Icon(Icons.library_books)),
+              Tab(icon: Icon(Icons.camera_alt_sharp))
+            ]),
+          ),
+          body: TabBarView(children: [
+            _panel,
+            const FullPublicationView(title: "Publications"),
+            const PolaroidGalleryView(title: "Polaroid Cameras"),
+            // const DatasetView(title: "Datasets"),
+          ])));
+}
+
+class DesktopLayout extends StatefulWidget {
+  final String titleEn;
+  final String titleZh;
+
+  const DesktopLayout({Key? key, required this.titleEn, required this.titleZh})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => DesktopLayoutState();
+}
+
+class DesktopLayoutState extends State<DesktopLayout> {
+  int _selectedIndex = 0;
+  final _body = [
+    Center(
+        child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 1200),
+            child: ListView(
+              children: const [
+                IntroductionCard(),
+                UpdateCard(),
+                PublicationCard(),
+                PolaroidCard()
+              ],
+            ))),
+    const FullPublicationView(title: "Publications"),
+    const PolaroidGalleryView(title: "Polaroid Cameras"),
+    // const DatasetView(title: "Datasets"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(widget.titleEn)),
+        body: Row(
+          children: [
+            NavigationRail(
+                selectedIndex: _selectedIndex,
+                labelType: NavigationRailLabelType.all,
+                onDestinationSelected: (int index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+                destinations: const [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Homepage'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.library_books),
+                    label: Text('Publications'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.camera_alt_sharp),
+                    label: Text('Polaroid'),
+                  ),
+                ]),
+            Expanded(
+              child: PageTransitionSwitcher(
+                transitionBuilder: (child, animation, secondaryAnimation) =>
+                    FadeThroughTransition(
+                  animation: animation,
+                  secondaryAnimation: secondaryAnimation,
+                  child: child,
+                ),
+                child: _body[_selectedIndex],
+              ),
+            )
+          ],
+        ));
+  }
 }
