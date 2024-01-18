ea8e3b85e6934686cd6e144495bc5361f1222cfa
jinzr <zengrui.jin0@gmail.com>
2024-01-18 14:22:54 +0800

    updated

--- io_page/lib/home_components/self_intro_card.dart
+++ io_page/lib/home_components/self_intro_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:zr_jin_page/utilities/author_name.dart';
 import 'package:zr_jin_page/utilities/link_buttonbar.dart';
 
@@ -11,17 +13,29 @@ class IntroductionCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
         child: Column(children: [
-      const ListTile(
-          leading: Image(
-              image: NetworkImage(
-                  "https://raw.githubusercontent.com/JinZr/flutter_io_page/main/io_page/assets/images/avatar/avatar.jpeg")),
-          title:
-              Text(AUTHOR_TEXT, style: TextStyle(fontWeight: FontWeight.bold)),
-          subtitle: Text("金增锐")),
+      ListTile(
+          leading: Image.network(
+              "https://raw.githubusercontent.com/JinZr/flutter_io_page/main/io_page/assets/images/avatar/avatar.jpeg"),
+          title: const Text(AUTHOR_TEXT,
+              style: TextStyle(fontWeight: FontWeight.bold)),
+          subtitle: const Text("金增锐")),
       const Divider(indent: 10),
-      const Column(children: [
-        ListTile(title: MyIntroduction()),
-        ListTile(title: SelectableText("zengrui.jin0 [at] gmail.com")),
+      Column(children: [
+        const ListTile(title: MyIntroduction()),
+        ListTile(
+          title: const Text("zengrui.jin0 [at] gmail.com"),
+          onTap: () {
+            Clipboard.setData(
+                    const ClipboardData(text: "zengrui.jin0@gmail.com"))
+                .catchError((e) {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text("Failed to write the address to clipboard.")));
+            }).then((value) {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text("Email address copied to clipboard.")));
+            });
+          },
+        ),
       ]),
       linkButtonBar
     ]));
