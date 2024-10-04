aa8394b3566fc8fc652d62146dd52452434c1f08
zr_jin <zengrui.jin0@gmail.com>
2024-10-04 22:46:12 +0800

    updated

--- io_page/build/web/flutter_bootstrap.js
+++ io_page/build/web/flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"3fbe87c088d33c894f1bf601370f607cea103b16","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"205484009711c2f0d85a4c45666d187184493b41","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3421725125"
+    serviceWorkerVersion: "3806817645"
   }
 });
