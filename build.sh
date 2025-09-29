8081e9a3ce9d7c5f339196c01a6b13f962e741af
zr_jin <peter.jin.cn@gmail.com>
2025-09-30 01:28:26 +0800

    updated

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # FILEPATH: /Users/zengruijin/Github Project/flutter_io_page/io_page/build.sh
-flutter build web --release --pwa-strategy none --wasm
+flutter build web \
+    --release \
+    --pwa-strategy none \
+    --optimization-level 4 
+
